7d9e74f1c11c56e4e09f923d0c181e1255dbdf7c
MehulDinesh <121483819+EgoisticPotato@users.noreply.github.com>
2024-04-18 11:26:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,31 @@
-from flask import Flask, render_template, Response
+from flask import Flask,render_template,Response
 import cv2
 
-app = Flask(__name__, template_folder='Templates')
+app=Flask(__name__)
+camera=cv2.VideoCapture(0)
 
-def generate_frames(camera):
+def generate_frames():
     while True:
-        success, frame = camera.read()
+            
+        ## read the camera frame
+        success,frame=camera.read()
         if not success:
             break
         else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
-        
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+            ret,buffer=cv2.imencode('.jpg',frame)
+            frame=buffer.tobytes()
+
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 @app.route('/video')
-def video_feed():
-    camera = cv2.VideoCapture(0)  # Use the correct camera index (0 or 1 depending on your setup)
-    return Response(generate_frames(camera), mimetype='multipart/x-mixed-replace; boundary=frame')
+def video():
+    return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
-if __name__ == "__main__":
+if __name__=="__main__":
     app.run(debug=True)
